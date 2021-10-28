1aa485ee3d1292c8b4192adf974493e7b06c78da
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-10-28 14:19:31 -0500

    Hashtag generator

---
+++ codewars-3.js
@@ -0,0 +1,37 @@
+// The Hashtag Generator
+// Here's the deal:
+// It must start with a hashtag (#).
+// All words must have their first letter capitalized.
+// If the final result is longer than 140 chars it must return false.
+// If the input or the result is an empty string it must return false.
+
+function generateHashtag(str) {
+  if (str !== "" && str.trim() !== "") {
+    let capped =
+      "#" +
+      str
+        .split(" ")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join("");
+    if (capped.length <= 140) {
+      return capped;
+    } else return false;
+  } else return false;
+}
+
+// console.log(
+//   "Expected:",
+//   false,
+//   "-----",
+//   "Returned:",
+//   generateHashtag(" ".repeat(200))
+// );
+
+// console.log(
+//   "Expected: #CodewarsIsNice",
+//   "-----",
+//   "Returned:",
+//   generateHashtag("Codewars is nice")
+// );
+
+// -----------------------------------------------------------------------------------------------------------------------
